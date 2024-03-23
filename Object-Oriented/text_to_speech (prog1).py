632fc4e7d79291dea0b256e42c50a497c2458637
Aidan Tiu <tiuprof77@gmail.com>
2024-03-23 17:36:21 +0800

    Add pseudocode

--- Object-Oriented/text_to_speech (prog1).py
+++ Object-Oriented/text_to_speech (prog1).py
@@ -1,3 +1,8 @@
+# Ask for user input
+# Convert the text to speech using the pyttsx3 init() function
+# Initialize functions to convert text to speech
+
+
 import pyttsx3
 
 text_speech = pyttsx3.init()
